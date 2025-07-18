8fbe318934fe335a6ceb8359ff814c3074fae139
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2025-07-18 10:25:26 +0200

    feat(app): first commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+// TODO: Remove this disable when Prisma client types are properly resolved
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
